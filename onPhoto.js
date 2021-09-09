7e75d4cfdeaee2147a1c530452653e5fddd572f1
Mr. Kutin <serge.kutin@gmail.com>
2021-09-09 15:40:36 +0300

    photo caption

--- onPhoto.js
+++ onPhoto.js
@@ -1,30 +1,38 @@
 const {getDb} = require('./database')
 
 const onPhoto = async ctx => {
-  const db = getDb(ctx.update.message.from.username)
-  if (!db) {
-    return
-  }
-
-  //todo try
-  //todo ask for caption if missing
-  const largestPhoto = ctx.update.message.photo.reduce((acc, photo) => {
-    if(photo.file_size > acc.file_size){
-      return photo
+  try {
+    if (!ctx.update.message.caption) {
+      ctx.reply('Нужно указать название для картинки, попробуйте ещё раз')
+      return
     }
-    return acc
-  }, {file_size: 0})
 
-  const text = ctx.update.message.caption
-  const tags = text?.split(' ') || []
+    const db = getDb(ctx.update.message.from.username)
+    if (!db) {
+      return
+    }
 
-  await db.post({
-    text,
-    tags,
-    content_type: 'image/png',
-    file_id: largestPhoto.file_id,
-    createdAt: new Date()
-  })
+    const largestPhoto = ctx.update.message.photo.reduce((acc, photo) => {
+      if (photo.file_size > acc.file_size) {
+        return photo
+      }
+      return acc
+    }, {file_size: 0})
+
+    const text = ctx.update.message.caption
+    const tags = text?.split(' ') || []
+
+    await db.post({
+      text,
+      tags,
+      content_type: 'image/png',
+      file_id: largestPhoto.file_id,
+      createdAt: new Date()
+    })
+    ctx.reply('Сохранила!')
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = onPhoto
