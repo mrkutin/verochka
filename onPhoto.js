ca95b7f9b84a9f65a30ad86b68a9f71b94e039d5
Mr. Kutin <serge.kutin@gmail.com>
2021-09-09 13:45:34 +0300

    file_id only

--- onPhoto.js
+++ onPhoto.js
@@ -1,37 +1,29 @@
-const https = require('https')
 const {getDb} = require('./database')
 
 const onPhoto = async ctx => {
+  const db = getDb(ctx.update.message.from.username)
+  if (!db) {
+    return
+  }
+
   //todo try
   //todo ask for caption if missing
-  const photoURL = await ctx.tg.getFileLink(ctx.update.message.photo[1].file_id)//todo get biggest file
+  const largestPhoto = ctx.update.message.photo.reduce((acc, photo) => {
+    if(photo.file_size > acc.file_size){
+      return photo
+    }
+    return acc
+  }, {file_size: 0})
 
-  https.get(photoURL, res => {
-    let photo = ''
-    res.setEncoding('base64')
+  const text = ctx.update.message.caption
+  const tags = text?.split(' ') || []
 
-    res.on('data', data => photo += data)
-    res.on('end', async () => {
-      const db = getDb(ctx.update.message.chat.username)
-      if (!db) {
-        return
-      }
-
-      const text = ctx.update.message.caption
-      const tags = text?.split(' ') || []
-
-      await db.post({
-        _attachments: {
-          'photo.png': {
-            content_type: 'image/png',
-            data: photo
-          }
-        },
-        text,
-        tags,
-        createdAt: new Date()
-      })
-    })
+  await db.post({
+    text,
+    tags,
+    content_type: 'image/png',
+    file_id: largestPhoto.file_id,
+    createdAt: new Date()
   })
 }
 
